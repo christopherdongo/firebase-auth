d24741015fe752892c10a641a5947f93df792c3b
christopherdongo <chris_d26@hotmail.com>
2025-09-18 14:44:43 -0500

    add config firebase enviroment entorno

--- src/config/firebase/Config.ts
+++ src/config/firebase/Config.ts
@@ -6,12 +6,12 @@ import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCgImrbMSLczvp9_0w6K9qJmy-nppksQHM",
-  authDomain: "react-auth-e68b8.firebaseapp.com",
-  projectId: "react-auth-e68b8",
-  storageBucket: "react-auth-e68b8.firebasestorage.app",
-  messagingSenderId: "425127661904",
-  appId: "1:425127661904:web:bded237289e6f361ae1c55"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
